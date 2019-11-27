339886292d86e6998cbdb084ed57597869cef243
AmreshVenugopal <amresh.venugopal@gmail.com>
2019-11-27 13:07:25 +0530

    update: site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: "/several-people-are-talking",
   siteMetadata: {
     title: `Several people are talking`,
-    description: `Podcast by vernacular.ai`,
+    description: `Audio archive for vernacular.ai`,
     author: ``,
   },
   plugins: [
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+          icon: `src/images/vernacular-ai-32x320.png`, // This path is relative to the root of the site.
       },
     },
       // this (optional) plugin enables Progressive Web App + Offline functionality
